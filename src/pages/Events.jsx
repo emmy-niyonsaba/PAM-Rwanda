8fc3009c7391ed2c15dc2309ccd250807b87c597
Emmy Niyonsaba <emmmyson5ku@gmail.com>
2026-03-27 12:38:53 +0200

    Update src/pages/Events.jsx

--- src/pages/Events.jsx
+++ src/pages/Events.jsx
@@ -27,7 +27,7 @@ export default function EventsPage() {
 
   return (
     <>
-      <Navbar />
+     
       
       <div className="min-h-screen bg-neutral-50 py-12">
         <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
