3b324aac63fa24850b9368c78b0501e926426eb5
Emmy Niyonsaba <emmmyson5ku@gmail.com>
2026-03-27 12:38:53 +0200

    Update src/pages/About.jsx

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -5,7 +5,6 @@ import Footer from '../components/Footer';
 export default function AboutPage() {
   return (
     <>
-      <Navbar />
 
       <div className="bg-neutral-50">
         {/* Header */}
