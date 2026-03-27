b0e0454c8cfe50d13b28f3602ae0fcef25e310a6
Emmy Niyonsaba <emmmyson5ku@gmail.com>
2026-03-27 12:07:51 +0200

    Update src/App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,43 @@
-import React from 'react'
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { useEffect } from 'react';
+import { initializeSocket } from './services/socket';
+
+// Pages
+import Home from './pages/Home';
+import Login from './pages/Login';
+import Register from './pages/Register';
+import Events from './pages/Events';
+import History from './pages/History';
+import Panafricanists from './pages/Panafricanists';
+import Opportunities from './pages/Opportunities';
+import Testimonies from './pages/Testimonies';
+import Dashboard from './pages/Dashboard';
+import About from './pages/About';
 
 function App() {
+  useEffect(() => {
+    // Initialize socket connection
+    initializeSocket();
+  }, []);
+
   return (
-  <>
-  
-  <h1 className=' bg-amber-800'>PAM-Rwanda Website intialization</h1>
-  </>
-  )
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/events" element={<Events />} />
+        <Route path="/history" element={<History />} />
+        <Route path="/panafricanists" element={<Panafricanists />} />
+        <Route path="/opportunities" element={<Opportunities />} />
+        <Route path="/testimonies" element={<Testimonies />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
-export default App
+export default App;
+
+
